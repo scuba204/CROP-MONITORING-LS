b74470cb0d779c6b460e8286f98ea5fb9bea4a1d
nkosinathe <nkosinatheleotla800@gmail.com>
2025-10-16 16:27:41 +0200

    added # TODO: Update end date as needed in anomaly detector model

--- TRAINING/train_anomaly_detector.py
+++ TRAINING/train_anomaly_detector.py
@@ -25,8 +25,8 @@ model_dir = "models"
 model_name = "crop_anomaly_detector.pkl"
 
 spectral_bands = ["B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B11", "B12"]
-training_start_date = "2025-07-26"
-training_end_date = "2025-08-02"
+training_start_date = "2025-07-26" # TODO: Update end date as needed
+training_end_date = "2025-08-02" # TODO: Update end date as needed
 
 # ==============================================================================
 # Helper Function for Feature Engineering
